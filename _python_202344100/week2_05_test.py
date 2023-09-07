344fade7de21b40046c4f0713981d54deabc0341
hyebbin__s2 <128661152+hyenbins2@users.noreply.github.com>
2023-09-07 19:40:12 +0900

    Add files via upload

---
+++ _python_202344100/week2_05_test.py
@@ -0,0 +1,39 @@
+
+# 슬라이싱
+mymajor = "컴퓨터정보" 
+print(mymajor[:])
+print(mymajor[0:5:2]) # 2개씩 점프해서 뽑기
+print(mymajor[0:5:1]) # 1개씩 점프해서 뽑기
+
+mymajor = "컴퓨터정보"
+print(mymajor[0:3])
+print(mymajor[1:3])
+print(mymajor[2:3])
+print(mymajor[:3]) # 시작 인덱스 없으면 처음부터 실행
+print(mymajor[3:5])
+print(mymajor[3:]) # 끝 인덱스 없으면 마지막까지 실행
+print(mymajor)
+
+mymajor = "컴퓨터정보"
+print(mymajor[-1])
+print(mymajor[0])
+print(mymajor[1])
+print(mymajor[2])
+print(mymajor[3])
+print(mymajor[4])
+print(mymajor[5])
+print(mymajor)
+
+mymajor = "컴퓨터정보"
+print(mymajor[-5])
+print(mymajor[-4])
+print(mymajor[-3])
+print(mymajor[-2])
+print(mymajor[-1])
+print(mymajor)
+
+
+print("=-" * 30)
+print(10 + 10)
+print("나이:" + "10")
+#print("나이:" + 10) 
